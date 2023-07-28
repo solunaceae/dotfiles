5228fea96f4a011b0d2b405b36fc25877de3a95c
Aman Luqman <aluqman@hsasupports-MacBook-Pro.local>
2023-07-28 09:57:50 -0500

    Migrate coq settings from VimScript -> Lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,11 +5,11 @@ return {
       { "ms-jpq/coq.artifacts" }
     },
     config = function()
-      vim.cmd([[
-        let g:coq_settings = { "auto_start": "shut-up",
-                             \ "keymap.jump_to_mark": "null",
-                             \ "keymap.pre_select": v:false }
-      ]])
+      vim.g.coq_settings = { 
+        ["auto_start"] = "shut-up",
+        ["keymap.jump_to_mark"] = "null",
+        ["keymap.pre_select"] = false 
+      }
 
       require("coq")
 
